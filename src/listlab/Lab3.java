77c1a3288d02686e54499bff93b4d76f24ffac28
tduwe <tduwe@L11710042773.wctc.msft>
2015-10-29 16:41:35 -0500

    Added Dog content from example to Lab 3

--- src/listlab/Lab3.java
+++ src/listlab/Lab3.java
@@ -10,5 +10,38 @@ package listlab;
  * @author tduwe
  */
 public class Lab3 {
+    private String name;
+    private int rabiesId;
+
+    public Lab3() {
+    }
+
+    public Lab3(String name, int rabiesId) {
+        this.name = name;
+        this.rabiesId = rabiesId;
+    }
     
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRabiesId() {
+        return rabiesId;
+    }
+
+    public void setRabiesId(int rabiesId) {
+        this.rabiesId = rabiesId;
+    }
+
+    
+    
+    public static void main(String[] args) {
+        Lab2 l = new Lab2("Duwe", "Thomas", "999-99-9999");
+        System.out.println(l);
+    }
 }
